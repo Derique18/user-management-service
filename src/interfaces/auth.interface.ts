91e964eaaf7176a65c95498b8babf4d27f50751d
Derique18 <ehizochrisderique@gmail.com>
2026-07-31 17:17:13 +0100

    feat: complete phase 4 interfaces and DTO's

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,35 @@
+export interface RegisterDTO {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface RefreshTokenDTO {
+  refreshToken: string;
+}
+
+export interface ForgotPasswordDTO {
+  email: string;
+}
+
+export interface ResetPasswordDTO {
+  token: string;
+  newPassword: string;
+}
+
+export interface JWTPayload {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
